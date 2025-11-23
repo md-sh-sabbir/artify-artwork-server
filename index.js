b42ac07f20e7b702c262fa6f5738c743cb91018a
md-sh-sabbir <sabbirhossain9870@gmail.com>
2025-11-24 00:57:14 +0600

    artworks and artists page post api has been added

--- index.js
+++ index.js
@@ -27,65 +27,116 @@ async function run() {
 
     const db = client.db("artwork-db");
     const artworksCollection = db.collection("artworks");
-    const artistsCollection = db.collection('artists')
+    const artistsCollection = db.collection("artists");
+    const favoritesCollection = db.collection("favorites");
 
-    app.get("/artworks", async(req, res) => {
-      const cursor = artworksCollection.find();
+    app.get("/artworks", async (req, res) => {
+      const query = {
+        visibility: "public",
+      };
+      const cursor = artworksCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get('/latest-artworks', async(req, res) => {
-      const cursor = artworksCollection.find().sort({created_at : -1}).limit(6)
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.post("/artworks", async (req, res) => {
+      const data = req.body;
+      const result = await artworksCollection.insertOne(data);
+      res.send(result);
+    });
 
-    app.get('/search', async(req, res) => {
-      const search_text = req.query.search 
-      const query = {}
-      if(search_text){
+    app.post("/artists", async (req, res) => {
+      const { name, photo, arts } = req.body;
+      const existingArtist = await artistsCollection.findOne({ name });
+
+      const query={
+        name: name
+      }
+
+      if (existingArtist) {
+        const update = {
+          $push: {
+            arts: arts[0],
+          },
+          $inc: {
+            total_artworks: 1,
+          },
+        };
+
+        const result = await artistsCollection.updateOne(query, update);
+        res.send(result)
+      } else {
+        const newArtist = {
+          name,
+          photo,
+          total_artworks: 1,
+          arts: arts
+        }
+        const result = await artistsCollection.insertOne(newArtist);
+        res.send(result);
+      }
+    });
+
+    app.get("/latest-artworks", async (req, res) => {
+      const cursor = artworksCollection
+        .find()
+        .sort({ created_at: -1 })
+        .limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/search", async (req, res) => {
+      const search_text = req.query.search;
+      const query = {};
+      if (search_text) {
         query.title = {
           $regex: search_text,
-          $options: "i"
-        }
+          $options: "i",
+        };
       }
-      const cursor = artworksCollection.find(query)
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+      const cursor = artworksCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/artworks/:id', async(req, res) => {
-      const id = req.params.id 
-      const query = {_id: new ObjectId(id)}
-      const result = await artworksCollection.findOne(query)
-      res.send(result)
-    })
+    app.get("/artworks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await artworksCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.get('/artworks/artist/:id', async(req, res) => {
-        const id = req.params.id 
+    app.get("/artworks/artist/:id", async (req, res) => {
+      const id = req.params.id;
 
-        const query = {_id: new ObjectId(id)}
-        const artwork = await artworksCollection.findOne(query)
+      const query = { _id: new ObjectId(id) };
+      const artwork = await artworksCollection.findOne(query);
 
-        const filter = {name: artwork.artist_name}
-        const artist = await artistsCollection.findOne(filter)
+      const filter = { name: artwork.artist_name };
+      const artist = await artistsCollection.findOne(filter);
 
-        res.send({artwork, artist})
-    })
+      res.send({ artwork, artist });
+    });
 
-    app.patch('/likes/:id', async(req, res) => {
-      const id = req.params.id 
-      // const data = req.body 
-      const query = {_id: new ObjectId(id)}
+    app.patch("/likes/:id", async (req, res) => {
+      const id = req.params.id;
+      // const data = req.body
+      const query = { _id: new ObjectId(id) };
       const update = {
-        $inc : {
-          likes_count: 1
-        }
-      }
-      const result = await artworksCollection.updateOne(query, update)
-      res.send(result)
-    })
+        $inc: {
+          likes_count: 1,
+        },
+      };
+      const result = await artworksCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    app.post("/favorites/:id", async (req, res) => {
+      const data = req.body;
+      const results = await favoritesCollection.insertOne(data);
+      res.send(results);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
